da4e688562464e0fc43bec4d5f172befa23e8f82
BURAK ÖĞRETİCİ <burakogreticii@gmail.com>
2022-01-07 11:52:52 +0300

    11:51 => Already Exists

--- Business/Concrete/ColorManager.cs
+++ Business/Concrete/ColorManager.cs
@@ -8,6 +8,8 @@ using Business.BusinessAspects;
 using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Performance;
 using Core.Aspects.Autofac.Validation;
+using System.Linq;
+using Core.Utilities.Business;
 
 namespace Business.Concrete
 {
@@ -20,11 +22,16 @@ namespace Business.Concrete
         {
             _colorDal = colorDal;
         }
-        
+
         //[SecuredOperation("color.add,admin")]
         [CacheRemoveAspect("IColorService.Get")]
         public IResult Add(Color color)
         {
+          IResult result = BusinessRules.Run(ColorAlReadyExists(color.ColorName));
+            if (result != null)
+            {
+                return result;
+            }
             _colorDal.Add(color);
             return new SuccessResult(Messages.ColorAdded);
 
@@ -36,28 +43,39 @@ namespace Business.Concrete
             _colorDal.Delete(color);
             return new Result(true, Messages.ColorDeleted);
         }
-       
-        
+
+
         [CacheAspect]
         [PerformanceAspect(5)]
         public IDataResult<List<Color>> GetAll()
         {
             return new SuccessDataResult<List<Color>>(_colorDal.GetAll(), Messages.ColorListed);
         }
-       
+
         [CacheAspect]
         [PerformanceAspect(5)]
         public IDataResult<Color> GetById(int colorId)
         {
             return new SuccessDataResult<Color>(_colorDal.Get(color => color.Id == colorId));
         }
-       
+
         [CacheRemoveAspect("IColorService.Get")]
         public IResult Update(Color color)
         {
             _colorDal.Update(color);
-            return new Result(true, Messages.ColorDeleted);
+            return new Result(true, Messages.ColorUpdated);
+        }
+
+
+        private IResult ColorAlReadyExists(string colorName)
+        {
+            var result = _colorDal.GetAll(c => c.ColorName == colorName).Any();
+            if (result == true)
+            {
+                return new ErrorResult(Messages.ColorAlreadyExists);
             }
+            return new SuccessResult();
+        }
 
     }
 }
